25f4ac5e4223e4ee0282a626621d11181067d413
shodai yamatsu <>
2023-05-19 19:50:39 +0900

    静的な検索ボタンの実装

--- vueproject/src/components/pages/FrontPage.vue
+++ vueproject/src/components/pages/FrontPage.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="background-wrapper wrapper">
     <div class="builderーarea">
-      <input class="room_search rounded bg-light text-dark py-1 mb-2" type="text" placeholder="部屋を探す">
+      <dl class="search">
+          <dt><input class="room_search rounded bg-light text-dark py-1 mb-2 rounded" type="text" placeholder="部屋を探す"></dt>
+          <dd><button><span></span></button></dd>
+      </dl>
       <input class="room_build rounded-pill bg-dark text-light py-1" type="button" value="部屋を作る">
     </div>
     <div class="userーarea">
@@ -21,4 +24,25 @@ export default {
 
 <!-- アプリホーム画面 -->
 <style scoped>
+dl.search{
+    width: 330px;
+    margin: auto;
+    position:relative;
+    background-color:#fff;
+}
+dl.search dd{
+    position:absolute;
+    top:0;
+    right:0;
+}
+dl.search dd button{
+    display:block;
+    padding:8px;
+}
+dl.search dd button span{
+    display:block;
+    width:20px;
+    height:20px;
+    background:url('../../assets//image/serch_con.svg') no-repeat scroll 0 0;
+}
 </style>
