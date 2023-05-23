73bcbed89a6eaf7b2537d48763c2e162c3b58615
shodai yamatsu <>
2023-05-23 09:38:04 +0900

    [Add] ユーザー情報のcomponent化

--- vueproject/src/components/modules/UserInfoBox.vue
+++ vueproject/src/components/modules/UserInfoBox.vue
@@ -1,14 +1,37 @@
 <template>
+    <div class="userInfo">
+        <img :src='img_pass' alt="ユーザー画像" class="userInfoImage">
+        <div>
+            <p>{{user_name}}</p>
+            <p>{{user_mail}}</p>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
     name: 'UserInfoBox',
     props: {
+        img_pass : {
+            type: String,
+            required: false
+        },
+        user_name : {
+            type: String,
+            required: false
+        },
+        user_mail	: {
+            type: String,
+            required: false
+        }
     }
 }
 </script>
 
 <!-- ユーザー情報を表示するモジュール -->
 <style scoped>
+.userInfo{
+    display: flex;
+    justify-content: center;
+}
 </style>
