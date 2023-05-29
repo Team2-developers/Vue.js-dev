76dc825adbc15165ced0fa270650f64646392be7
YuSa0-6 <fuku.sugar@icloud.com>
2023-05-29 14:29:51 +0900

    [UPDATE] Create FooterNav

---
+++ vueproject/src/components/modules/FooterNav.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="card fixed-bottom text-center h-56">
+    <ul class="nav d-flex card-footer p-0 text-white">
+      <li class="nav-item flex-fill">
+        <a href="#" class="nav-link active">
+          <img src="../../assets/image/heart-icon.svg" alt="Feed" srcset="">
+          <div>Feed</div>
+        </a>
+      </li>
+      <li class="nav-item flex-fill">
+        <a href="#" class="nav-link active">
+          <img src="../../assets/image/heart-icon.svg" alt="Feed" srcset="">
+          <div>Search</div>
+        </a>
+      </li>
+      <li class="nav-item flex-fill">
+        <a href="#" class="nav-link active">
+          <img src="../../assets/image/heart-icon.svg" alt="Feed" srcset="">
+          <div>Notifications</div>
+        </a>
+      </li>
+      <li class="nav-item flex-fill">
+        <a href="#" class="nav-link active">
+          <img src="../../assets/image/heart-icon.svg" alt="Feed" srcset="">
+          <div>Me</div>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'FooterNav',
+    props: {
+    }
+}
+</script>
+
+<!-- フッター固定のナビゲーション -->
+<style scoped>
+</style>
