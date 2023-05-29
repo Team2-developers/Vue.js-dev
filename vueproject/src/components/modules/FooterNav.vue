9ccd29e9147d3f9139dc764d351fe982a3e7016f
YuSa0-6 <fuku.sugar@icloud.com>
2023-05-29 16:51:02 +0900

    [CREATE] add icons

--- vueproject/src/components/modules/FooterNav.vue
+++ vueproject/src/components/modules/FooterNav.vue
@@ -1,27 +1,27 @@
 <template>
-  <div class="card fixed-bottom text-center h-56">
+  <div id="appFooterNav" class="card fixed-bottom text-center h-56">
     <ul class="nav d-flex card-footer p-0 text-white">
       <li class="nav-item flex-fill">
-        <a href="#" class="nav-link active">
-          <img src="../../assets/image/heart-icon.svg" alt="Feed" srcset="">
+        <a href="/" class="nav-link active">
+          <img src="../../assets/image/home-icon.svg" alt="Feed" srcset="">
           <div class="text-dark">Feed</div>
         </a>
       </li>
       <li class="nav-item flex-fill">
-        <a href="#" class="nav-link active">
-          <img src="../../assets/image/heart-icon.svg" alt="Search" srcset="">
+        <a href="/gamesearch" class="nav-link active">
+          <img src="../../assets/image/search-icon.svg" alt="Search" srcset="">
           <div class="text-dark">Search</div>
         </a>
       </li>
       <li class="nav-item flex-fill">
-        <a href="#" class="nav-link active">
-          <img src="../../assets/image/heart-icon.svg" alt="Notifications" srcset="">
+        <a href="/notificationPanel" class="nav-link active">
+          <img src="../../assets/image/bell-icon.svg" alt="Notifications" srcset="">
           <div class="text-dark">Notifications</div>
         </a>
       </li>
       <li class="nav-item flex-fill">
-        <a href="#" class="nav-link active">
-          <img src="../../assets/image/heart-icon.svg" alt="Me" srcset="">
+        <a href="/profilePage" class="nav-link active">
+          <img src="../../assets/image/vector-icon.svg" alt="Me" srcset="">
           <div class="text-dark">Me</div>
         </a>
       </li>
@@ -34,7 +34,7 @@ export default {
     name: 'FooterNav',
     props: {
     }
-}
+  }
 </script>
 
 <!-- フッター固定のナビゲーション -->
