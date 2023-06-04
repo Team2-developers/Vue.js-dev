4c9d29e629f3c9b73089738702258f8484a6153d
shodai yamatsu <>
2023-06-04 17:10:54 +0900

    [Add] componentの作成と呼び出し

--- vueproject/src/components/pages/GameDisplay.vue
+++ vueproject/src/components/pages/GameDisplay.vue
@@ -1,11 +1,30 @@
 <template>
+<div class="background-wrapper wrapper">
+  <SpeechBubble
+    :img_pass="img_pass"
+    :user_name="user_name"
+    :life_name	="life_name"
+  />
+</div>
 </template>
 
 <script>
+import SpeechBubble from '@/components/modules/SpeechBubble'
+
 export default {
   name: 'GameDisplay',
+  data(){
+    return{
+      img_pass: 'user_noImage.svg',
+      user_name: 'ochinpo',
+      life_name: '山田の人生'
+    }
+  },
   props: {
-  }
+  },
+  components: {
+    SpeechBubble
+  },
 }
 </script>
 
