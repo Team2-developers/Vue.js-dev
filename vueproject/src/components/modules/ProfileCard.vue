43db9dd3d1f9fad2715970bf91f638dc65b70cff
YuSa0-6 <fuku.sugar@icloud.com>
2023-06-04 16:33:36 +0900

    [Update] add component/ProfileCard

---
+++ vueproject/src/components/modules/ProfileCard.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="profile_card">
+        <p>ProfileCard</p>
+        <p>UserName</p>
+        <table class="table table-borderless">
+            <thead>
+            <tr>
+                <th scope="col"></th>
+                <th scope="col"></th>
+            </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">生年月日</th>
+                    <td>6月30日</td>
+                </tr>
+                <tr>
+                    <th scope="row">血液型</th>
+                    <td>A型</td>
+                </tr>
+                <tr>
+                    <th scope="row">趣味</th>
+                    <td>######</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProfileCard',
+    props: {
+    }
+}
+</script>
+
+<!-- フッター固定のナビゲーション -->
+<style scoped>
+.profile_card{
+    background-image: url(../../assets/image/profile_card_backimg.jpg)
+}
+</style>
