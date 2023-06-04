d70d6213123c41f0bf0822ff954e6f2b7ffbaf40
shodai yamatsu <>
2023-06-04 16:32:25 +0900

    [Start] ゲーム画面のrouteの追加

--- vueproject/src/router/index.js
+++ vueproject/src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import FrontPage from '@/components/pages/FrontPage'
 import NotificationPanel from '@/components/pages/NotificationPanel'
 import ProfilePage from '@/components/pages/ProfilePage'
+import GameDisplay from '@/components/pages/GameDisplay'
 
 const routes = [
     { path: '/', name: 'FrontPage', component: FrontPage },
     { path: '/notificationPanel', name: 'NotificationPanel', component: NotificationPanel },
-    { path: '/profilePage', name: 'ProfilePage', component: ProfilePage }
+    { path: '/profilePage', name: 'ProfilePage', component: ProfilePage },
+    { path: '/gameDisplay', name: 'GameDisplay', component: GameDisplay }
 ]
 
 const router = createRouter({
