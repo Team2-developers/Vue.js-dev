c0b1c9256e84d15bfbd1bd150dcdae4633000e63
shodai yamatsu <>
2023-06-05 19:55:46 +0900

    [Add] GameListモジュールの作成

--- vueproject/src/components/modules/GameList.vue
+++ vueproject/src/components/modules/GameList.vue
@@ -1,14 +1,61 @@
 <template>
+    <div class="gameList">
+        <img :src="getImageLifePath" alt="人生サムネ">
+        <div class="gameListIcon">
+            <img :src="getImagePath" alt="人生の作者">
+            <p>{{life_name}}</p>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
     name: 'GameList',
     props: {
+        img_pass : {
+            type: String,
+            required: true
+        },
+        life_img : {
+            type: String,
+            required: false
+        },
+        life_name : {
+            type: String,
+            required: false
+        },
+        life_path : {
+            type: String,
+            required: false
+        }
+    },
+    computed: {
+        getImagePath() {
+            return require('@/assets/image/' + this.img_pass);
+        },
+        getImageLifePath() {
+            return require('@/assets/image/' + this.life_img);
+        }
     }
 }
 </script>
 
 <!-- 作成したゲーム(作製未も含む)を表示するためのモジュール -->
 <style scoped>
+.gameList{
+    max-width: 200px;
+}
+.gameListIcon{
+    display: flex;
+    justify-content: space-between;
+    height: 40px;
+}
+.gameListIcon img{
+    height: 100%;
+}
+.gameListIcon p{
+    line-height: 40px;
+    height: 100%;
+    vertical-align: middle;
+}
 </style>
--- vueproject/src/router/index.js
+++ vueproject/src/router/index.js
@@ -3,12 +3,14 @@ import FrontPage from '@/components/pages/FrontPage'
 import NotificationPanel from '@/components/pages/NotificationPanel'
 import ProfilePage from '@/components/pages/ProfilePage'
 import GameDisplay from '@/components/pages/GameDisplay'
+import GameSearch from '@/components/pages/GameSearch'
 
 const routes = [
     { path: '/', name: 'FrontPage', component: FrontPage },
     { path: '/notificationPanel', name: 'NotificationPanel', component: NotificationPanel },
     { path: '/profilePage', name: 'ProfilePage', component: ProfilePage },
-    { path: '/gameDisplay', name: 'GameDisplay', component: GameDisplay }
+    { path: '/gameDisplay', name: 'GameDisplay', component: GameDisplay },
+    { path: '/gameSearch', name: 'GameSearch', component: GameSearch }
 ]
 
 const router = createRouter({
