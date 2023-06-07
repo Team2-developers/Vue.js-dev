c1d251c5b4eaa8f1854f365797ad34ed73a7e65d
m2101239 <134896482+m2101239@users.noreply.github.com>
2023-06-07 12:00:13 +0900

    Revert "遷移の際のスクロール位置の変更"

--- vueproject/src/router/index.js
+++ vueproject/src/router/index.js
@@ -14,18 +14,8 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
- scrollBehavior(to, from, savedPosition){
-    if (savedPosition) {
-      return savedPosition
-    } else { 
-      return {
-        top: 0
-      }
-    }
-  }
-
+    history: createWebHistory(),
+    routes,
 })
 
 export default router
