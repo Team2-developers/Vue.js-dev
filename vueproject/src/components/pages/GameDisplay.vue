013d8e2a44b584e42eab6b247ca95e3e5b077f49
2101239 <2101239@gmail.com>
2023-06-16 10:27:01 +0900

    アラート表示をモーダル表示に変更

--- vueproject/src/components/pages/GameDisplay.vue
+++ vueproject/src/components/pages/GameDisplay.vue
@@ -3,7 +3,7 @@
   <SpeechBubble
     :img_pass="img_pass"
     :user_name="user_name"
-    :life_name	="life_name"
+    :life_name="life_name"
   />
   <div class="gameTurn">
       <!-- スコア -->
@@ -13,18 +13,32 @@
       </div>
       <!-- 順番 -->
       <div>
-        <p>山田太郎</p>
+        <p>{{this.userName[this.currentUserIndex]}}</p>
       </div>
       <!-- 残りマス -->
       <div>
-        <p>30/30</p>
+        <p>{{this.userArray[this.currentUserIndex].length==0 ? "29"
+            :this.userArray[this.currentUserIndex].reduce((a, b) => a - b, 29)}}</p>
         <p>残りマス</p>
       </div>
     </div>
-</div>
-<div class="gameArea">
-  <img src="../../assets/image/lifeGameBoard.svg" alt="">
-</div>
+    <div class="gameArea">
+      <!-- class属性は共通部分と各クラスの初期値を分離して作成しています -->
+      <img class="gameAreaimage" src="../../assets/image/lifeGameBoard.svg" alt="">
+      <img v-for="(position, index) in userPositions" :key="index"
+      :class="`gameUser gameUser${index+1}`"
+      :style="{ top: `${position[1]}px`, left: `${position[0]}px` }"
+      :src="`${userImages[index]}`"
+      >
+    </div>
+    <button class="rollDice" @click="rollDice">サイコロを振る</button>
+  </div>
+    <div v-if="isModalOpen" class="modal">
+      <div class="modal-content">
+        <span class="close" @click="closeModal">&times;</span>
+        <p style="text-align:center">出た数は...<br>{{ this.diceValue }}</p>
+      </div>
+    </div>
 </template>
 
 <script>
@@ -36,13 +50,66 @@ export default {
     return{
       img_pass: 'user_noImage.svg',
       user_name: 'ochinpo',
-      life_name: '山田の人生'
+      life_name: '山田の人生',
+      userArray: [[], [], [], []],
+      currentUserIndex: 0,
+      assocArray : { "1": [60, 0], "2": [120, 0], "3": [170, 0], "4": [220, 0], "5": [270, 0], "6": [260, 50], "7": [260, 100], "8": [260, 150], "9": [260, 200], "10": [260, 250], "11": [260, 300], "12": [260, 350], "13": [210, 350], "14": [160, 350], "15": [110, 350], "16": [60, 350], "17": [10, 350], "18": [10, 300], "19": [10, 250], "20": [10, 200], "21": [10, 150], "22": [10, 100], "23": [60, 100], "24": [110, 100], "25": [160, 100], "26": [160, 150], "27": [160, 200], "28": [160, 250], "29": [100, 250] },
+      initialPosition: { "gameUser1": [10, 12], "gameUser2": [30, 12], "gameUser3": [10, 36], "gameUser4": [30, 36] },
+      userImages:[
+        require("../../assets/image/icon1.svg"),
+        require("../../assets/image/icon2.svg"),
+        require("../../assets/image/icon3.svg"),
+        require("../../assets/image/icon4.svg")
+      ], 
+      diceValue: 0,
+      isModalOpen: false,
+      userName: [
+        "dog","cat","pig","sheep"
+      ]
     }
   },
   props: {
   },
   components: {
     SpeechBubble
+  },
+methods: {
+  rollDice() {
+    // 振ったサイコロで進む数
+    let diceValue = Math.floor(Math.random() * 6) + 1;
+    this.openModal(diceValue)
+    // 各ユーザーの進んだマスの数を保存
+    this.userArray[this.currentUserIndex].push(diceValue);
+    // ユーザーを次に人に変更
+    this.currentUserIndex = (this.currentUserIndex + 1) % 4;
+  }, // Add a comma here
+
+  openModal(diceValue) {
+    this.isModalOpen = true;
+    this.diceValue = diceValue;
+   // 3秒後にモーダルを非表示にする
+  setTimeout(() => {
+    this.isModalOpen = false;
+    }, 2500);
+  },
+    closeModal() {
+       this.isModalOpen = false;
+  },
+},
+   computed: {
+    totalDiceValues() {
+      return this.userArray.map(array => array.reduce((a, b) => a + b, 0));
+    },
+    // 現在の位置を取得
+    userPositions() {
+      // valueの中に各ユーザーの情報が組み込まれています
+      return this.totalDiceValues.map((value, index) => {
+        let position = this.initialPosition[`gameUser${index+1}`];
+        // 今回移動したマス目の情報を取得して新規位置の設定
+        const movedPosition = this.assocArray[value] || [0, 0];
+        return [position[0] + movedPosition[0], position[1] + movedPosition[1]];
+      });
+    },
   },
 }
 </script>
@@ -67,7 +134,71 @@ export default {
   margin-bottom: 0;
 }
 .gameArea{
+  max-width: 320px;
   text-align: center;
-  margin-top: 30px;
+  margin: 30px auto 0;
+  position: relative;
+  z-index: 1;
+}
+.rollDice{
+  border-radius: 15px;
+  margin-top: 20px;
+  padding: 10px;
+  background: black;
+  color: white;
+}
+.gameUser{
+  z-index: 10;
+  position: absolute;
+  width: 20px;
+}
+.gameUser1{
+  top: 12px;
+  left: 10px;
+}
+.gameUser2{
+  top: 12px;
+  left: 30px;
+}
+.gameUser3{
+  top: 36px;
+  left: 10px;
+}
+.gameUser4{
+  top: 36px;
+  left: 30px;
+}
+.modal {
+  display: block;
+  position: fixed;
+  z-index: 1;
+  left: 0%;
+  top: 0%;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.modal-content {
+  background-color: #fff;
+  margin: 25% auto;
+  padding: 10px;
+  border: 1px solid #888;
+  width: 30%;
+}
+
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
 }
 </style>
