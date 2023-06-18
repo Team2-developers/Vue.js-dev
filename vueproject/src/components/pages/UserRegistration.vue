5a6a08831139bd4f106f036a84b7bdaaacbb3e20
YuSa0-6 <fuku.sugar@icloud.com>
2023-06-18 15:18:08 +0900

    [update] rename Registration->UserRegistration to resolve name error

---
+++ vueproject/src/components/pages/UserRegistration.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="background-wrapper wrapper">
+    <FooterNav />
+  </div>
+</template>
+
+<script>
+import FooterNav from '../modules/FooterNav.vue';
+
+export default {
+    name: "UserRegistration",
+    props: {},
+    components: { FooterNav }
+}
+</script>
+
+<!-- 新規登録画面 -->
+<style scoped>
+</style>
