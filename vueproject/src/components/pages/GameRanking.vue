a8a6b00de7142b47ee29b415af981d2b1b4386bb
YuSa0-6 <fuku.sugar@icloud.com>
2023-06-18 15:15:20 +0900

    [update] rename Ranking->GameRanking to resovle name error

---
+++ vueproject/src/components/pages/GameRanking.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="background-wrapper wrapper">
+    <FooterNav />
+  </div>
+</template>
+
+<script>
+import FooterNav from '../modules/FooterNav.vue';
+
+export default {
+    name: "GameRanking",
+    props: {},
+    components: { FooterNav }
+}
+</script>
+
+<!-- ランキング画面 -->
+<style scoped>
+</style>
