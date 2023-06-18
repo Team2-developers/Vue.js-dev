0ae93bef97a3b8a06e446df38db113de1ea8c495
YuSa0-6 <fuku.sugar@icloud.com>
2023-06-18 15:20:38 +0900

    [update] rename route's name of gameRanking, userLogin and userRegistration

--- vueproject/src/router/index.js
+++ vueproject/src/router/index.js
@@ -4,15 +4,15 @@ import GameDetails from '@/components/pages/GameDetails'
 import GameDisplay from '@/components/pages/GameDisplay'
 import GameModification from '@/components/pages/GameModification'
 import GameSearch from '@/components/pages/GameSearch'
+import GameRanking from '@/components/pages/GameRanking'
 import GameStart from '@/components/pages/GameStart'
-import Login from '@/components/pages/Login'
 import NotificationPanel from '@/components/pages/NotificationPanel'
 import ProfilePage from '@/components/pages/ProfilePage'
 import ProfileUpdate from '@/components/pages/ProfileUpdate'
-import Ranking from '@/components/pages/Ranking'
-import Registration from '@/components/pages/Registration'
 import RoomCreationComplete from '@/components/pages/RoomCreationComplete'
 import RoomSearch from '@/components/pages/GameSearch'
+import UserLogin from '@/components/pages/UserLogin'
+import UserRegistration from '@/components/pages/UserRegistration'
 
 const routes = [
     { path: '/', name: 'FrontPage', component: FrontPage },
@@ -20,15 +20,15 @@ const routes = [
     { path: '/gameDisplay', name: 'GameDisplay', component: GameDisplay },
     { path: '/gameModification', name: 'GameModification', component: GameModification },
     { path: '/gameSearch', name: 'GameSearch', component: GameSearch },
+    { path: '/gameRanking', name: 'GameRanking', component: GameRanking },
     { path: '/gameStart', name: 'GameStart', component: GameStart },
-    { path: '/login', name: 'Login', component: Login },
     { path: '/notificationPanel', name: 'NotificationPanel', component: NotificationPanel },
     { path: '/profilePage', name: 'ProfilePage', component: ProfilePage },
     { path: '/profileUpdate', name: 'ProfileUpdate', component: ProfileUpdate },
-    { path: '/ranking', name: 'Ranking', component: Ranking },
-    { path: '/registration', name: 'Registration', component: Registration },
     { path: '/roomCreationComplete', name: 'RoomCreationComplete', component: RoomCreationComplete },
     { path: '/roomSearch', name: 'RoomSearch', component: RoomSearch },
+    { path: '/userLogin', name: 'UserLogin', component: UserLogin },
+    { path: '/userRegistration', name: 'UserRegistration', component: UserRegistration },
 ]
 
 const router = createRouter({
