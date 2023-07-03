11b6c6f20e1841464476eccdf4df6bf3fba7d0da
shodai yamatsu <>
2023-07-03 11:54:12 +0900

    [Add] ログイン・新規作成・プロフィールカードのapi認証

--- vueproject/src/components/pages/UserLogin.vue
+++ vueproject/src/components/pages/UserLogin.vue
@@ -1,34 +1,83 @@
 <template>
   <div class="background-wrapper wrapper">
-            <div class="userーarea">
-    <div class="centered-container">
-      <input class="user_name rounded bg-light text-dark py-1 mb-2 rounded" type="text" placeholder="ユーザー名" style="width: 300px;">
-      <div class="spacer"></div> 
-      <input class="user_password rounded bg-light text-dark py-1 mb-2 rounded" type="text" placeholder="パスワード" style="width: 300px;">
-      <div class="spacer"></div> 
-      <input class="login rounded-pill bg-dark text-light py-1" type="button" value="ログイン" style="width: 150px;">
-      <div class="spacer"></div> 
-          </div>
-    </div>
+    <form @submit.prevent="submitLogin">
+      <div class="userーarea">
+        <div class="centered-container">
+          <input
+            class="user_name rounded bg-light text-dark py-1 mb-2 rounded"
+            type="text"
+            placeholder="ユーザー名"
+            style="width: 300px"
+            v-model="user_mail"
+          />
+          <div class="spacer"></div>
+          <input
+            class="user_password rounded bg-light text-dark py-1 mb-2 rounded"
+            type="password"
+            placeholder="パスワード"
+            style="width: 300px"
+            v-model="password"
+          />
+          <div class="spacer"></div>
+          <input
+            class="login rounded-pill bg-dark text-light py-1"
+            type="submit"
+            value="ログイン"
+            style="width: 150px"
+          />
+          <div class="spacer"></div>
+        </div>
+      </div>
+    </form>
     <FooterNav />
   </div>
 </template>
 
 <script>
-import FooterNav from '../modules/FooterNav.vue';
+import axios from "axios";
+import FooterNav from "../modules/FooterNav.vue";
 
 export default {
-  name: 'LoginPage',
+  name: "LoginPage",
   data() {
     return {
+      user_mail: "",
+      password: "",
       img_pass: "user_noImage.svg",
       user_name: "ochinpo",
-      user_mail: "test@gmail.com",
     };
   },
   props: {},
   components: {
     FooterNav,
+  },
+  methods: {
+    async submitLogin() {
+      try {
+        const response = await axios.post("http://localhost:8000/api/login", {
+          user_mail: this.user_mail,
+          password: this.password,
+        });
+        if (response.status === 200) {
+          alert("ログイン成功");
+          localStorage.setItem('auth_token', response.data.token);
+          this.$router.push('/ProfilePage');
+          // You can also set user data or tokens here, based on the response
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+  mounted() {
+    if (localStorage.user_mail) {
+      this.user_mail = localStorage.getItem("user_mail");
+    }
+  },
+  watch: {
+    user_mail(newMail) {
+      localStorage.user_mail = newMail;
+    }
   },
 };
 </script>
@@ -43,6 +92,6 @@ export default {
 }
 
 .spacer {
-  height: 10px; 
+  height: 10px;
 }
 </style>
--- vueproject/src/components/pages/UserRegistration.vue
+++ vueproject/src/components/pages/UserRegistration.vue
@@ -1,19 +1,117 @@
 <template>
-  <div class="background-wrapper wrapper">
-    <FooterNav />
+  <div>
+    <form @submit.prevent="submitImage">
+      <div>
+        <label>File: </label>
+        <input type="file" @change="onFileChange" />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+
+    <form @submit.prevent="submitForm">
+      <div>
+        <label>Email: </label>
+        <input type="email" v-model="user.user_mail" required />
+      </div>
+      <div>
+        <label>Name: </label>
+        <input type="text" v-model="user.user_name" required />
+      </div>
+      <div>
+        <label>Password: </label>
+        <input type="password" v-model="user.password" required />
+      </div>
+      <div>
+        <label>Birth: </label>
+        <input type="date" v-model="user.birth" />
+      </div>
+      <div>
+        <label>Blood Type: </label>
+        <input type="text" v-model="user.blood_type" />
+      </div>
+      <div>
+        <label>hobby </label>
+        <input type="text" v-model="user.hobby" />
+      </div>
+      <div v-for="i in 5" :key="i">
+        <label>Episode {{ i }}: </label>
+        <input type="text" v-model="user['episode' + i]" />
+      </div>
+
+      <button type="submit">Submit</button>
+    </form>
   </div>
 </template>
 
 <script>
-import FooterNav from '../modules/FooterNav.vue';
+import axios from "axios";
 
 export default {
-    name: "UserRegistration",
-    props: {},
-    components: { FooterNav }
-}
-</script>
+  data() {
+    return {
+      user: {
+        img_id: "",
+        user_mail: "",
+        user_name: "",
+        password: "",
+        birth: "",
+        blood_type: "",
+        hobby: "",
+        episode1: "",
+        episode2: "",
+        episode3: "",
+        episode4: "",
+        episode5: "",
+      },
+      file: null,
+    };
+  },
+  methods: {
+    onFileChange(e) {
+      this.file = e.target.files[0];
+    },
 
-<!-- 新規登録画面 -->
-<style scoped>
-</style>
+    async submitImage() {
+      try {
+        const formData = new FormData();
+        formData.append("file", this.file);
+
+        const response = await axios.post(
+          "http://localhost:8000/api/fileupload",
+          formData,
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          }
+        );
+
+        if (response.status === 200) {
+          alert("保存完了");
+          this.user.img_id = response.data.img_id;
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    },
+
+    async submitForm() {
+      try {
+        const response = await axios.post(
+          "http://localhost:8000/api/user/create",
+          this.user
+        );
+
+        if (response.status === 201) {
+          this.$emit("user-created", response.data.user);
+          localStorage.setItem("user_mail", response.data.user.user_mail);
+          alert("アカウント作成成功");
+          this.$router.push("/UserLogin");
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+};
+</script>
