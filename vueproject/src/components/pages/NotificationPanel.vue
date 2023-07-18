fcf599ce18d8bb063b2361f6f43cc537043a7436
shodai yamatsu <>
2023-07-19 02:47:21 +0900

    通知機能の取得

--- vueproject/src/components/pages/NotificationPanel.vue
+++ vueproject/src/components/pages/NotificationPanel.vue
@@ -2,35 +2,53 @@
   <div class="background-wrapper wrapper">
     <h2>通知</h2>
     <NotificationBanner
-        :img_pass="img_pass"
-        :user_name="user_name"
+      v-for="notification in notifications"
+      :key="notification.user_id"
+      :img_pass="notification.img_path"
+      :user_name="notification.user_name"
+      :user_email="notification.user_email"
+      :comment="notification.message"
     />
     <FooterNav />
   </div>
 </template>
 
 <script>
-import NotificationBanner from '@/components/modules/NotificationBanner'
-import FooterNav from '../modules/FooterNav.vue';
+import axios from "axios";
+import NotificationBanner from "@/components/modules/NotificationBanner";
+import FooterNav from "../modules/FooterNav.vue";
 
 export default {
-  name: 'NotificationPanel',
-  data () {
+  name: "NotificationPanel",
+  data() {
     return {
-        img_pass: "user_noImage.svg",
-        user_name: "ochinpo",
-        comment: "あなたの人生に「コメント」しました"
-    }
-  },
-  props: {
+      notifications: [],
+    };
   },
   components: {
     NotificationBanner,
-    FooterNav
-}
-}
+    FooterNav,
+  },
+  mounted() {
+    let token = localStorage.getItem("auth_token");
+    axios
+      .get(`http://localhost:8000/api/notifications`, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          this.notifications = response.data.notification;
+          console.log(response.data)
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
 </script>
 
 <!-- 通知画面 -->
-<style scoped>
-</style>
+<style scoped></style>
